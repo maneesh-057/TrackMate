00a780e294304a26d8b6889321d78038bbd33e13
Maneesh Kumar <yadavmanish0572@gmail.com>
2025-05-26 01:46:51 +0530

    Update TaskService.java

--- TaskService.java
+++ TaskService.java
@@ -26,6 +26,7 @@ public class TaskService {
             return;
         }
         for (Task task : tasks) {
+            System.out.println("--------------------------------");
             System.out.println(tasks);
         }
     }
