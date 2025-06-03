5f91b93cdbd69fc427c1df685aa9cfecaa3069d0
Maneesh Kumar <yadavmanish0572@gmail.com>
2025-06-04 00:23:22 +0530

    Update For Review2.java

--- Task.java
+++ Task.java
@@ -1,9 +1,9 @@
 public class Task {
-    int id;
-    String title;
-    String description;
-    String dueDate;
-    boolean isCompleted;
+    private int id;
+    private String title;
+    private String description;
+    private String dueDate;
+    private boolean isCompleted;
 
     public Task(int id, String title, String description, String dueDate, boolean isCompleted) {
         this.id = id;
@@ -38,16 +38,21 @@ public class Task {
     }
 
     public void printTask() {
-        System.out.println("Task ID: " + id);
-        System.out.println("Title: " + title);
-        System.out.println("Description: " + description);
-        System.out.println("Due Date: " + dueDate);
-        System.out.println("Completed: " + (isCompleted ? "Yes" : "No"));
+        System.out.println("------------ Task Details ------------");
+        System.out.println("Task ID      : " + id);
+        System.out.println("Title        : " + title);
+        System.out.println("Description  : " + description);
+        System.out.println("Due Date     : " + dueDate);
+        System.out.println("Completed    : " + (isCompleted ? "Yes" : "No"));
+        System.out.println("--------------------------------------");
     }
 
     @Override
     public String toString() {
-        return "Task ID: " + id + "\nTitle: " + title + "\nDescription: " + description +
-                "\nDue Date: " + dueDate + "\nCompleted: " + (isCompleted ? "Yes" : "No");
+        return "Task ID: " + id +
+               " | Title: " + title +
+               " | Description: " + description +
+               " | Due Date: " + dueDate +
+               " | Completed: " + (isCompleted ? "Yes" : "No");
     }
 }
